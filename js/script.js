cf249d0f63ed05c3cb3f8696b45f295ea665951b
Marie-France <83244931+mariefrancelng@users.noreply.github.com>
2025-03-06 18:55:33 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const langSelector = document.getElementById("lang-select");
     const elementsToTranslate = document.querySelectorAll("[data-i18n]");
 
-    // Mise à jour du texte en fonction de la langue choisie
+
     function updateLanguage(lang) {
-        localStorage.setItem("lang", lang); // Sauvegarde la langue choisie
+        localStorage.setItem("lang", lang); 
         elementsToTranslate.forEach(el => {
             const key = el.getAttribute("data-i18n");
             if (translations[lang] && translations[lang][key]) {
@@ -13,13 +13,17 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Vérification si langue enregistrée
     const savedLang = localStorage.getItem("lang") || "fr";
     langSelector.value = savedLang;
     updateLanguage(savedLang);
 
-    // Changement de la langue
     langSelector.addEventListener("change", function () {
         updateLanguage(this.value);
     });
+});
+
+const discoverButton = document.getElementById('discover-button');
+
+discoverButton.addEventListener('click', function() {
+    window.location.href = 'src/home.html';
 });
